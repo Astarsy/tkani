e42d93d05d652e713cae640ea556db0d96a80ef0
Mikhail Gladkov <astarsy@mail.ru>
2016-06-16 20:26:05 +0300

    Уточнена схема роутинга

--- controllers/DefaultController.class.php
+++ controllers/DefaultController.class.php
@@ -1,14 +1,24 @@
 <?php
 class DefaultController{
     //Контроллер поумолчанию, выводит основной контент
+    public function errorMethod(){
+    	// запрос несуществующего метода Любого контроллера
+        $fc=AppController::getInstance();
+        $fc->setContent($fc->render('error.twig.html'));
+    }
     public function Method(){
     	// gladkov.loc
         $fc=AppController::getInstance();
         $fc->setContent($fc->render('index.twig.html'));
     }
-    public function errorMethod(){
-    	// запрос несуществующего метода Любого контроллера
+    public function goodMethod(){
+    	// gladkov.loc/good/3
         $fc=AppController::getInstance();
-        $fc->setContent($fc->render('error.twig.html'));
+        $fc->setContent($fc->render('show_good.twig.html'));
+    }
+    public function cathMethod(){
+    	// gladkov.loc/cath/3
+        $fc=AppController::getInstance();
+        $fc->setContent($fc->render('show_cath.twig.html'));
     }
 }
